b80f2b7b47928d568237afe4445442007697f317
Tanmay Vaij <tanmayvaij22@gmail.com>
2025-05-16 21:55:57 +0530

    removed user module
    Signed-off-by: Tanmay Vaij <tanmayvaij22@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
